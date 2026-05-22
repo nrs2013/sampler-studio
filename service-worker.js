35e06974f6bf65f2ba077443b261b9428b9f40c3
野村裕紀 <nomura0913@gmail.com>
2026-05-22 09:58:25 +0900

    v12.5.4: 色付きパッドのバッジ背景四角もチントで統一感
    N44/M バッジの背景四角 (ダーク #14141A) が選んだ色と分離して見え
    ガチャついていた。color-mix で 18% (通常) / 32% (hover) の pad-color
    チントを乗せて統一感を出す。

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@
 // 注意: 音源データ (blob URL / OPFS / プロジェクトフォルダ) は SW では扱わない。
 // SW はあくまでアプリ本体 (HTML/JS/CSS) のキャッシュのみ。
 
-const SW_CACHE_NAME = 'sampler-studio-v12.5.3';
+const SW_CACHE_NAME = 'sampler-studio-v12.5.4';
 const APP_SHELL = [
   './',
   './index.html',
